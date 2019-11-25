772dd88554f571647de820f03e0190a982a7cdf1
zhanglize <Z18434395962@163.com>
2019-11-25 12:20:16 +0800

    add(hailiying)：新增海利赢商场项目代码

---
+++ Plugins/Nop.Plugin.Payments.HygainsA/Controllers/PaymentAlipayController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+using Nop.Core;
+using Nop.Core.Domain.Payments;
+using Nop.Plugin.Payments.HygainsA.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Orders;
+using Nop.Services.Payments;
+using Nop.Web.Framework.Controllers;
+
+namespace Nop.Plugin.Payments.HygainsA.Controllers
+{
+    public class PaymentHygainsAController : BasePaymentController
+    {
+
+
+        #region Ctor
+
+        public PaymentHygainsAController()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure()
+        {
+            var model = new ConfigurationModel
+            {
+
+            };
+
+            return View("~/Plugins/Payments.HygainsA/Views/Configure.cshtml", model);
+        }
+
+        [HttpPost]
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure(ConfigurationModel model)
+        {
+            if (!ModelState.IsValid)
+                return Configure();
+
+            //save settings
+
+            return Configure();
+        }
+
+        [ChildActionOnly]
+        public ActionResult PaymentInfo()
+        {
+            return View("~/Plugins/Payments.HygainsA/Views/PaymentInfo.cshtml");
+        }
+
+        [NonAction]
+        public override IList<string> ValidatePaymentForm(FormCollection form)
+        {
+            var warnings = new List<string>();
+
+            return warnings;
+        }
+
+        [NonAction]
+        public override ProcessPaymentRequest GetPaymentInfo(FormCollection form)
+        {
+            var paymentInfo = new ProcessPaymentRequest();
+
+            return paymentInfo;
+        }
+
+
+        #endregion
+    }
+}
---
+++ Plugins/Nop.Plugin.Payments.HygainsA/RouteProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.Payments.HygainsA
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        #region Methods
+
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            //Notify
+            routes.MapRoute("Plugin.Payments.HygainsA.Notify",
+                 "Plugins/PaymentHygainsA/Notify",
+                 new { controller = "PaymentHygainsA", action = "Notify" },
+                 new[] { "Nop.Plugin.Payments.HygainsA.Controllers" }
+            );
+
+            //Notify
+            routes.MapRoute("Plugin.Payments.HygainsA.Return",
+                 "Plugins/PaymentHygainsA/Return",
+                 new { controller = "PaymentHygainsA", action = "Return" },
+                 new[] { "Nop.Plugin.Payments.HygainsA.Controllers" }
+            );
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        #endregion
+    }
+}
