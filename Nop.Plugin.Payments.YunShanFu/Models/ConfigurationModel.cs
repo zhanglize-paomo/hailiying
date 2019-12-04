3e7a457ae15a7e34d1f6961c420570ce9c1a82e5
zhanglize <Z18434395962@163.com>
2019-12-04 17:28:25 +0800

    update

---
+++ Nop.Plugin.Payments.YunShanFu/Models/ConfigurationModel.cs
@@ -0,0 +1,10 @@
+﻿using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc;
+
+namespace Nop.Plugin.Payments.HygainsA.Models
+{
+    public class ConfigurationModel : BaseNopModel
+    {
+
+    }
+}
---
+++ Nop.Plugin.Payments.YunShanFu/RouteProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.Payments.YunShanFu
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        #region Methods
+
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            //Notify
+            routes.MapRoute("Plugin.Payments.YunShanFu.Notify",
+                 "Plugins/PaymentHygainsA/Notify",
+                 new { controller = "YunShanFu", action = "Notify" },
+                 new[] { "Nop.Plugin.Payments.HygainsA.Controllers" }
+            );
+
+            //Notify
+            routes.MapRoute("Plugin.Payments.YunShanFu.Return",
+                 "Plugins/PaymentHygainsA/Return",
+                 new { controller = "YunShanFu", action = "Return" },
+                 new[] { "Nop.Plugin.Payments.YunShanFu.Controllers" }
+            );
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        #endregion
+    }
+}
